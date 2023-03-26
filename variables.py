aed4e8acd6bce092516256fe6a98aaf45fb2b163
Jack Krolik <94269474+jack-krolik@users.noreply.github.com>
2023-03-26 12:30:38 -0400

    added comments

--- variables.py
+++ variables.py
@@ -4,9 +4,23 @@ import numpy as np
 
 
 class TaVariables():
+    """
+    Class for exporting ta attributes
+    """
+
+    # dataframe for ta csv and sections csv
     ta = pd.read_csv('tas.csv')
     sections = pd.read_csv('sections.csv')
+
+    # max number of sections ta can be assigned to as a list
     max_assigned = ta['max_assigned'].values
+
+    # weekday and time of each section as a numpy array
     times = sections['daytime'].to_numpy()
+
+    # minimum # of tas needed for each section as a list
     minimum_support = sections['min_ta'].values
+
+    # the preferences of each ta as a list
     prefs = ta.loc[:, '0':].values
+
